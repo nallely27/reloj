e67736422c63a7b73096322296e3089c50f70e91
Nallely <nalle2708@hotmail.com>
2013-02-24 21:14:39 -0600

    es un reloj sencillo visto en pantalla

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading; //necesario para la pausa
+
+
+namespace reloj_en_consola
+{
+    class Program
+    {
+        int seg = 0;       
+        int min = 0;       
+        int hor = 0;      
+        private void timer1_Tick(object sender, EventArgs e)
+        {           
+            if (min == 61)            
+            {            
+                hor += 1;
+                    min = 0;  
+            }            
+            if (seg == 61)
+            {             
+                min += 1;                
+                seg = 0;           
+            }
+            seg += 1;         
+                System.Console.WriteLine(hor.ToString() + ":" + min.ToString() + ":" + seg.ToString());    
+        }
+        static void Main(string[] args)
+        {
+            Console.Clear(); //limpia la consola
+            Console.WriteLine(DateTime.Now); //imprime la fecha y hora actual del sistema
+            Thread.Sleep(1000); //genera una pausa en el programa de 
+        }
+    }
+}
